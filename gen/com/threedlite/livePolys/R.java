ca87aab64209677e6d125d65cc273a90c39464c1
dan <dan_meany@yahoo.com>
2012-09-05 16:36:18 -0500

    add icon

--- gen/com/threedlite/livePolys/R.java
+++ gen/com/threedlite/livePolys/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher_wallpaper=0x7f020000;
+        public static final int threedlite512=0x7f020001;
     }
     public static final class string {
         public static final int wallpaper_threedlite=0x7f040000;
